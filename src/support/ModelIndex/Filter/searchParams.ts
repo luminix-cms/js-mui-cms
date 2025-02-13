b0dcadfab851f0058d8be67390ae5107284f58d4
Bruno Polo <brunopolo@poli.ufrj.br>
2025-02-13 16:38:52 -0300

    feat: refactor translateColumnsFromQuery

--- src/support/ModelIndex/Filter/searchParams.ts
+++ src/support/ModelIndex/Filter/searchParams.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { DateTime, Obj, Query, Str } from '@luminix/support';
+import { DateTime, Obj, Query } from '@luminix/support';
 import { app } from '@luminix/core';
 
 import { FilterColumn, FilteredColumn } from '../../../types/Filter';
@@ -15,89 +15,148 @@ import { FilterColumn, FilteredColumn } from '../../../types/Filter';
  * @param {any} value
  * @returns 
  */
-export const translateColumnsFromQuery = (columns: FilterColumn[], searchParams: any) => {
+export function translateColumnsFromQuery(columns: FilterColumn[], searchParams: any): FilteredColumn[] {
 
-    const FilterFacade = app('filter');
+    // const FilterFacade = app('filter');
 
-    const params = Query.toObject(searchParams);
+    // const params = Query.toObject(searchParams);
 
-    const queryObject = Object.keys(params).reduce((acc: any, key) => {
-        if ([ 'where' ].includes(key)) {
-            acc[key] = params[key];
-        }
-        return acc;
-    }, {});
+    // const queryObject = Object.keys(params).reduce((acc: any, key) => {
+    //     if ([ 'where' ].includes(key)) {
+    //         acc[key] = params[key];
+    //     }
+    //     return acc;
+    // }, {});
+
+    // const filteredColumns: FilteredColumn[] = [];
+
+    // // TODO: review typing of forEach parameter
+    // Object.entries(queryObject).forEach(([, params]: any) => {
+    //     // TODO: review typing of forEach parameter 
+    //     Object.entries(params).forEach(([operation, value]: any) => {
+    //         columns.filter((column) => {
+
+    //             const operator = (() => {
+    //                 const op = Str.camel(operation.split(Str.camel(column.key))[1]);
+
+    //                 if (Obj.isEmpty(op)) {
+    //                     return 'equals';
+    //                 }
+    //                 return op;
+    //             })();
+
+    //             if (!FilterFacade.getOperators().includes(operator)) {
+    //                 return false;
+    //             }
+
+    //             const attribute = Str.snake(operation.split(Str.ucfirst(operator))[0]);
+
+    //             return column.key === attribute;
+    //         })
+    //             .forEach((column) => {
+
+    //                 let operator = Str.camel(operation.split(Str.camel(column.key))[1]);
+
+    //                 if (typeof operator === 'undefined' || operator === null || Obj.isEmpty(operator)) {
+    //                     operator = 'equals';
+    //                 }
+
+    //                 let _value = value;
+    
+    //                 switch (FilterFacade.getInputType(column.type)) {
+    //                     case 'text': {
+    //                         if (Array.isArray(value)) {
+    //                             _value = value.join(',');
+    //                         }
+    //                         break;
+    //                     }
+    //                     case 'datetime-local': {
+    //                         if (Array.isArray(value)) {
+    //                             _value = value.map((v) => DateTime.toDateTimeLocal(v));
+    //                         } else {
+    //                             _value = DateTime.toDateTimeLocal(value);
+    //                         }
+
+    //                         break;
+    //                     }
+    //                     case 'boolean': {
+    //                         _value = Number(value);
+    //                         break;
+    //                     }
+    //                 }
+                    
+    //                 filteredColumns.push({
+    //                     key: column.key, 
+    //                     operator, 
+    //                     type: column.type, 
+    //                     value: _value, 
+    //                     nullable: column.nullable,
+    //                     is_relation: column.is_relation, 
+    //                 });
+    //             });
+    //     });
+    // });
+            
+    // return filteredColumns;
 
     const filteredColumns: FilteredColumn[] = [];
+    const queryObject = Query.toObject(searchParams);
 
-    // TODO: review typing of forEach parameter
-    Object.entries(queryObject).forEach(([, params]: any) => {
-        // TODO: review typing of forEach parameter 
-        Object.entries(params).forEach(([operation, value]: any) => {
-            columns.filter((column) => {
+    if (!('where' in queryObject) || Obj.isEmpty(queryObject.where)) {
+        return [];
+    }
 
-                const operator = (() => {
-                    const op = Str.camel(operation.split(Str.camel(column.key))[1]);
+    Object.entries(queryObject.where).forEach(([key, value]: any) => {
 
-                    if (Obj.isEmpty(op)) {
-                        return 'equals';
-                    }
-                    return op;
-                })();
+        // eslint-disable-next-line prefer-const
+        let [attribute, operator = 'equals'] = key.split(':');
 
-                if (!FilterFacade.getOperators().includes(operator)) {
-                    return false;
+        const column = columns.find((column) => column.key === attribute);
+
+        if (!column) {
+            return;
+        }
+
+        if (operator === 'equals' && column.is_relation) {
+            operator = 'relation';
+        }
+
+        let _value = value;
+
+        switch (column.type) {
+            case 'text': {
+                if (Array.isArray(value)) {
+                    _value = value.join(',');
+                }
+                break;
+            }
+            case 'datetime-local': {
+                if (Array.isArray(value)) {
+                    _value = value.map((v) => DateTime.toDateTimeLocal(v));
+                } else {
+                    _value = DateTime.toDateTimeLocal(value);
                 }
 
-                const attribute = Str.snake(operation.split(Str.ucfirst(operator))[0]);
+                break;
+            }
+            case 'boolean': {
+                _value = Number(value);
+                break;
+            }
+        }
 
-                return column.key === attribute;
-            })
-                .forEach((column) => {
-
-                    let operator = Str.camel(operation.split(Str.camel(column.key))[1]);
-
-                    if (typeof operator === 'undefined' || operator === null || Obj.isEmpty(operator)) {
-                        operator = 'equals';
-                    }
-
-                    let _value = value;
-    
-                    switch (FilterFacade.getInputType(column.type)) {
-                        case 'text': {
-                            if (Array.isArray(value)) {
-                                _value = value.join(',');
-                            }
-                            break;
-                        }
-                        case 'datetime-local': {
-                            if (Array.isArray(value)) {
-                                _value = value.map((v) => DateTime.toDateTimeLocal(v));
-                            } else {
-                                _value = DateTime.toDateTimeLocal(value);
-                            }
-
-                            break;
-                        }
-                        case 'boolean': {
-                            _value = Number(value);
-                            break;
-                        }
-                    }
-                    
-                    filteredColumns.push({
-                        key: column.key, 
-                        operator, 
-                        type: column.type, 
-                        value: _value, 
-                        nullable: column.nullable,
-                        is_relation: column.is_relation, 
-                    });
-                });
+        filteredColumns.push({
+            key: column.key, 
+            operator, 
+            type: column.type, 
+            value: _value, 
+            nullable: column.nullable,
+            is_relation: column.is_relation, 
         });
     });
-            
+
     return filteredColumns;
+
 }
 
 /**
